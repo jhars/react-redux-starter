b376af858c76710272b5b402ca3401a7bf3eff13
Jonathan Harlan <jjharlan06@gmail.com>
2016-08-08 11:31:15 -0400

    1st Commit

--- 8. react-redux/app/ui/aside-container.js
+++ 8. react-redux/app/ui/aside-container.js
@@ -3,10 +3,30 @@ import store from 'store';
 import { connect } from 'react-redux';
 import Aside from './aside';
 
-const stateToProps = function(state) {
-    return {
-        hasUsers: state.userReducer.users.length ? true : false
-    }
+//ORIGINAL
+// const stateToProps = function(state) {
+//     return {
+//         hasUsers: state.userReducer.users.length ? true : false
+//     }
+// }
+
+// export default connect(stateToProps)(Aside);
+
+const AsideContainer = React.createClass({
+	render: function() {
+		return (
+			<Aside hasUsers={this.props.hasUsers} /> // hasUsers becomes the State
+		)
+	}
+});
+
+// React React is good for the Subscription part of thingds, not the dispatch part
+
+const storeSelector = function (store) {
+	console.log(store);
+	return {
+		hasUsers: store.userReducer.users.length ? true : false
+	}
 }
 
-export default connect(stateToProps)(Aside);
+export default connect(storeSelector)(AsideContainer)
